0536dfe0c4aa5385f0fe156f21a9910329365987
Gouthum Kharvi <gouthumkharvi9845@gmail.com>
2026-04-05 22:01:46 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical-chatbot-genai",
+    version="0.1.0",
+    author="Gouthum kharvi",
+    author_email="gouthumkharvi1899@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
