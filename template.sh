f6c6c06bc070f4e9e5766b72018a29391fb47511
Gouthum Kharvi <gouthumkharvi9845@gmail.com>
2026-04-05 18:07:48 +0530

    Folder Structure Added

---
+++ template.sh
@@ -0,0 +1,16 @@
+#CREATING A DIRECTORY 
+mkdir -p src
+mkdir -p reserach 
+
+#CREATING THE FILES
+touch src/__init__.py
+touch src/__helper__.py
+touch src/__prompt__.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+
+echo "Directory and files created successfully!"
